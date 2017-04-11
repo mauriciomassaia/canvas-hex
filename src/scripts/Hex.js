495e4d7aaaead30984729d5a75a623d1f490023d
Grumpy Mauricio <grumpymauricio@mm.local>
2017-04-11 17:29:05 +1000

    Add lines following each hex element, wip.

--- src/scripts/Hex.js
+++ src/scripts/Hex.js
@@ -1,26 +1,31 @@
 import { Graphics } from 'pixi.js';
 import { TweenMax, Linear } from 'gsap';
 
-export default class Hex extends Graphics {
-  
-  constructor() {
-    super();
+export default class Hex {
+
+  constructor(stage, initX, initY) {
+    this.stage = stage;
     this.distance = 20;
     this.angleInc = Math.PI / 3;
     this.angle = Math.PI;
-    this.beginFill(0x333333, 1);
-    this.drawCircle(0, 0, 4);
-    this.endFill();
+
+    this.g = new Graphics();
+    this.g.beginFill(0x333333, 1);
+    this.g.drawCircle(0, 0, 4);
+    this.g.endFill();
+    this.g.x = initX;
+    this.g.y = initY;
     this.run = this.run.bind(this);
+    this.stage.addChild(this.g);
   }
 
   run() {
     const i = (Math.random() > 0.5) ? 1 : -1;
     this.angle += this.angleInc * i;
     this.alpha -= 0.015;
-    this.tween = TweenMax.to(this, .2, {
-      x: Math.sin(this.angle) * this.distance + this.x,
-      y: Math.cos(this.angle) * this.distance + this.y,
+    this.tween = TweenMax.to(this.g, 0.2, {
+      x: (Math.sin(this.angle) * this.distance) + this.g.x,
+      y: (Math.cos(this.angle) * this.distance) + this.g.y,
       ease: Linear.easeInOut,
       onComplete: this.run
     });
@@ -28,6 +33,8 @@ export default class Hex extends Graphics {
 
   kill() {
     this.tween.kill();
-    this.parent.removeChild(this);
+    this.stage.removeChild(this.g);
+    this.stage = null;
+    this.g = null;
   }
 }
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,9 @@
+import { Application, RenderTexture } from 'pixi.js';
 import '../styles/index.css';
-import { Application } from 'pixi.js';
 import Hex from './Hex';
 
 const MAX_ALIVE = 500;
-const app = new PIXI.Application(window.innerWidth, window.innerHeight, {
+const app = new Application(window.innerWidth, window.innerHeight, {
   backgroundColor: 0xffffff,
   antialias: true,
   resolution: window.devicePixelRatio || 1,
@@ -13,11 +13,13 @@ document.body.appendChild(app.view);
 
 const list = [];
 setInterval(() => {
-  const hex = new Hex();
-  hex.x = app.renderer.width / 2;
-  hex.y = app.renderer.height / 2;
+  const hex = new Hex(
+    app.stage,
+    app.renderer.width / 2,
+    app.renderer.height / 2
+  );
   hex.run();
-  app.stage.addChild(hex);
+  // app.stage.addChild(hex);
   if (list.length === MAX_ALIVE) {
     list.shift().kill();
   }
@@ -25,6 +27,8 @@ setInterval(() => {
   list.push(hex);
 }, 30);
 
-window.addEventListener('load', () => {
-  console.log('window ready.');
-});
+// app.ticker.add(() => {
+//   const renderTex = RenderTexture.create(app.renderer, app.renderer.width, app.renderer.height);
+//   app.renderer.render(app.stage, renderTex, false);
+
+// });
