580a3cf166dbc7a52d5151949cbe7e5a1fc87604
Mauricio Massaia <mauriciomassaia@gmail.com>
2020-04-26 21:36:54 +1000

    Update interaction

--- src/Hex.js
+++ src/Hex.js
@@ -2,14 +2,14 @@ import { Graphics } from 'pixi.js'
 import { TweenMax, Linear } from 'gsap'
 
 export default class Hex extends Graphics {
-  constructor(initX, initY, color) {
+  constructor (initX, initY, color, time = 0.1) {
     super()
     this.distance = 20
     this.angleInc = Math.PI / 3
     this.angle = Math.PI
     this.x = initX
     this.y = initY
-    this.time = 0.1
+    this.time = time
 
     this.cacheAsBitmap = true
     this.beginFill(color, 1)
@@ -19,7 +19,7 @@ export default class Hex extends Graphics {
     this.run = this.run.bind(this)
   }
 
-  run() {
+  run () {
     const direction = Math.random() > 0.5 ? 1 : -1
     this.angle += this.angleInc * direction
     this.tween = TweenMax.to(this, this.time, {
@@ -30,7 +30,7 @@ export default class Hex extends Graphics {
     })
   }
 
-  kill() {
+  kill () {
     this.tween.kill()
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,42 +1,42 @@
 import { Application, filters, Container, Graphics, RenderTexture, Sprite } from 'pixi.js'
 import Hex from './Hex'
 
+const COLORS = [0xC9BFBF, 0xBDB4F0, 0xFF9999, 0xFF6B6B, 0xfbcb00, 0x7a7a78]
+const HEX_TIME = [0.05, 0.1, 0.2, 0.6]
 const MAX_ALIVE = 200
-const COLOR_CHANGE_TIME = 2000
-const ADD_PARTICLE_TIME = 1000 / 20
+const PI2 = Math.PI * 2
 
-const COLORS = [
-  0xC9BFBF,
-  0xBDB4F0,
-  0xFF9999,
-  0xFF6B6B,
-  0xfbcb00,
-  0x7a7a78
-]
+const hexPaticles = []
+const center = {
+  x: window.innerWidth >> 1,
+  y: window.innerHeight >> 1
+}
 
-let currentColor = 0;
-const hexPaticles = [];
-console.log(window.innerWidth, window.innerHeight)
+let useColors = false
+let frameCounter = 0
+let currentColor = 0
+let numFramesPerParticle = 2
+let hexTimeIndex = 2
+let maxDistance = center.x < center.y ? center.x : center.y
+
 const app = new Application({
   width: window.innerWidth,
   height: window.innerHeight,
   backgroundColor: 0xffffff,
   antialias: true,
-  roundPixels: true,
-  resolution: window.devicePixelRatio || 1,
-  view: document.body.getElementsByTagName('canvas')[0]
+  resolution: window.devicePixelRatio || 1
 })
 document.body.appendChild(app.view)
+app.view.className = 'home-canvas'
 
 const container = new Container()
-
-const blurFilter = new filters.BlurFilter(2, 2)
+const blurFilter = new filters.BlurFilter(4, 4)
 const renderTex = RenderTexture.create(app.renderer.width, app.renderer.height)
 const sprite = new Sprite(renderTex)
 
 // white background will fade out the drawing behind.
 const bg = new Graphics()
-bg.beginFill(0xffffff, 0.1)
+bg.beginFill(0xffffff, 0.05)
 bg.drawRect(0, 0, app.renderer.width, app.renderer.height)
 bg.endFill()
 
@@ -44,17 +44,27 @@ app.stage.filters = [blurFilter]
 app.stage.addChild(sprite)
 app.stage.addChild(container)
 
-app.ticker.add(() => {
+app.stage.interactive = true
+
+function render () {
+  frameCounter++
+
+  if (frameCounter >= numFramesPerParticle) {
+    addParticle()
+    frameCounter = 0
+  }
+
   // update trail
   app.renderer.render(bg, renderTex, false)
   app.renderer.render(container, renderTex, false)
-})
+}
 
-const addParticle = () => {
+function addParticle () {
   const hex = new Hex(
     window.innerWidth / 2,
     window.innerHeight / 2,
-    COLORS[currentColor]
+    useColors ? COLORS[currentColor] : 0x000000,
+    HEX_TIME[hexTimeIndex]
   )
   container.addChild(hex)
   hex.run()
@@ -67,16 +77,66 @@ const addParticle = () => {
   hexPaticles.push(hex)
 }
 
-const changeColor = () => {
-  currentColor++
-  currentColor %= COLORS.length
+function getAngle (p1, p2) {
+  const a = p2.x - p1.x
+  const b = p2.y - p1.y
+  var angle = Math.atan2(b, a)
+  if (angle < 0) {
+    angle += 2 * Math.PI
+  }
+  return angle
 }
 
-setInterval(addParticle, ADD_PARTICLE_TIME)
-setInterval(changeColor, COLOR_CHANGE_TIME)
+function getDistance (p1, p2) {
+  const a = p2.x - p1.x
+  const b = p2.y - p1.y
+  return Math.sqrt((a * a) + (b * b))
+}
 
-window.addEventListener('resize', () => {
+function onWindowPointerMove (e) {
+  const pt = { x: e.clientX, y: e.clientY }
+  const distance = Math.min(getDistance(center, pt), maxDistance)
+  const angle = getAngle(center, pt)
+
+  // change hexTime based on distance, the closer the faster
+  hexTimeIndex = Math.round((distance / maxDistance) * (HEX_TIME.length - 1))
+
+  // change current color based on mouse angle to center of screen
+  currentColor = Math.round((angle / PI2) * (COLORS.length - 1))
+
+  // based on hexTimeIndex, so the closed to the center , the faster to add particles
+  numFramesPerParticle = (hexTimeIndex * 0.5) + 1
+}
+
+function onWindowPointerUp () {
+  useColors = !useColors
+}
+
+function onWindowResize () {
+  const w = window.innerWidth
+  const h = window.innerHeight
+
+  center.x = w >> 1
+  center.y = h >> 1
+
+  maxDistance = center.x < center.y ? center.x : center.y
+
+  // resize renderTexture and baseRenderTexture
+  renderTex.resize(w, h, true)
+  // resize bg
+  bg.width = w
+  bg.height = h
+  // resize renderer
+  app.renderer.resize(w, h)
+
+  // render again and clear buffer
   app.renderer.render(bg, renderTex, true)
   app.renderer.render(container, renderTex, true)
-  app.renderer.resize(window.innerWidth, window.innerHeight)
+}
+
+window.addEventListener('load', () => {
+  window.addEventListener('resize', onWindowResize)
+  window.addEventListener('pointermove', onWindowPointerMove)
+  window.addEventListener('pointerup', onWindowPointerUp)
+  app.ticker.add(() => render())
 })
